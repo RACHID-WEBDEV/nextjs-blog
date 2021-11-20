abd4d972cd5e7ce11f8fec374b4141a1f6521ceb
rachid-webdev <rasheedolamide2015@gmail.com>
2021-11-20 17:24:16 +0100

    final commit/ responsiveness

--- components/Author.jsx
+++ components/Author.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { grpahCMSImageLoader } from "../util";
 
 const Author = ({ author }) => (
-     <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+     <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-[#f9f9f9] bg-opacity-20">
           <div className="absolute left-0 right-0 -top-14">
                <Image
                     unoptimized
